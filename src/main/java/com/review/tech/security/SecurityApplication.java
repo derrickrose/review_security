1fb162432b37fc97b11b4e6bb3da9f52ce31dd2e
AMPILAHY Frelin (Canal Plus Prestataire) <frelin.ampilahy@cpexterne.org>
2021-05-27 23:05:58 +0200

    update code

--- src/main/java/com/review/tech/security/SecurityApplication.java
+++ src/main/java/com/review/tech/security/SecurityApplication.java
@@ -1,13 +1,13 @@
-package com.review.tech.security;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SecurityApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class, args);
-	}
-
-}
+//package com.review.tech.security;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class SecurityApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(SecurityApplication.class, args);
+//	}
+//
+//}
